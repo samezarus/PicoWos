1a643d320eed43d01a23a03af529029ef78de53c
samezarus <samezarus@gmail.com>
2024-02-07 23:21:18 +1000

    class Request

--- main.py
+++ main.py
@@ -1,18 +1,59 @@
-# Complete project details at https://RandomNerdTutorials.com/raspberry-pi-pico-web-server-micropython/
-
 import network
 import socket
 import time
 
-#import json
 import ujson as json
 
 from machine import Pin, I2C
 
 import ubinascii
 
-# HTU21D /// https://github.com/flrrth/pico-htu21d
-from htu21d import HTU21D
+
+def val_from_str(key_val: str) -> str:
+    l = key_val.split(': ')
+    return l[1] if len(l) == 2 else ''
+
+
+def url_params(raw_url: str) -> {}:
+    result = {
+        "url": "",
+        "params": {}
+    }
+
+    if "?" not in raw_url:
+        result["url"] = raw_url
+    else:
+        l = raw_url.split("?")
+        result["url"] = l[0]
+
+        ll = l[1].split("&")
+        if len(ll) > 0:
+            for item in ll:
+                if "=" in item:
+                    lll = item.split("=")
+                    result["params"][lll[0]] = lll[1]
+
+    return result
+
+
+class Request():
+    def __init__(self, byte_request) -> None:
+        r = byte_request.decode("utf-8").split('\r\n')
+
+        rr = r[0].split(' ')
+
+        self.method: str = rr[0]
+        self.url: dict = url_params(rr[1])
+        self.ver: str = rr[2]
+
+        self.host: str = val_from_str(r[1])
+        self.connection: str = val_from_str(r[2])
+        self.upgrade_insecure_requests: str = val_from_str(r[3])
+        self.user_agent: str = val_from_str(r[4])
+        self.accept: str = val_from_str(r[5])
+        self.referer: str = val_from_str(r[6])
+        self.accept_encoding: str = val_from_str(r[7])
+        self.accept_language: str = val_from_str(r[8])
 
 
 # HTML template for the webpage
@@ -56,15 +97,8 @@ def save_conf() -> bool:
 
 conf = load_conf()
 
-urls = {
-    '/led': None
-}
-
-# Initialize variables
-state = "OFF"
-
-# Create an LED object on pin 'LED'
-led = Pin('LED', Pin.OUT)
+def_led = Pin('LED', Pin.OUT)
+def_led_state = "OFF"
 
 # Wi-Fi credentials
 wlan_ssid = conf['wifi']['ssid']
@@ -113,28 +147,38 @@ while True:
         
         # Receive and parse the request
         request = conn.recv(1024)
+        
+        # raw_request = request.decode("utf-8").split('\r\n')
+        # print(raw_request)
+        
+        r = Request(request)
+        # print(r.url)
+        # print(url_params(r.url))
+        print(r.url)
+
+        
         request = str(request)
 
         try:
             request = request.split()
-            print(request)
-
+            
             request = request[1]
-            print('Request:', request)
+            # print('Request:', request)
         except IndexError as e:
             print(f'IndexError: {e}')
         
         # Process the request and update variables
         if request == '/lighton?':
             print("LED on")
-            led.value(1)
-            state = "ON"
+            def_led.value(1)
+            def_led_state = "ON"
         elif request == '/lightoff?':
-            led.value(0)
-            state = 'OFF'
+            print("LED off")
+            def_led.value(0)
+            def_led_state = 'OFF'
 
         # Generate HTML response
-        response = webpage(state)  
+        response = webpage(def_led_state)  
 
         # Send the HTTP response and close the connection
         conn.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
